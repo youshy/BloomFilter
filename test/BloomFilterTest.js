a6ea3d5b521226d89df98c93aad12be407fdf239
Artur <devilyoushy@gmail.com>
2019-01-05 12:16:37 +0000

    Small tests touchup

--- test/BloomFilterTest.js
+++ test/BloomFilterTest.js
@@ -1,24 +1,17 @@
 const assert = require("chai").assert;
 const BloomFilter = require("../BloomFilter");
 
-const bloom = new BloomFilter(150);
-
 describe("BloomFilter", () => {
-  describe("add", () => {
-    it("Should add member to storage", () => {
-      bloom.add("Tom");
-      assert.equal(bloom.contains("Tom"), true);
-    });
-  });
-
   describe("contains", () => {
     it("Should return true if there is a probability of member being in the storage", () => {
+      let bloom = new BloomFilter(150);
       bloom.add("Tom");
       bloom.add("Jerry");
       bloom.add("Coyote");
       assert.equal(bloom.contains("coyote"), true);
     });
     it("Should return false if the member is not in the storage", () => {
+      let bloom = new BloomFilter(150);
       bloom.add("Tom");
       bloom.add("Jerry");
       bloom.add("Coyote");
@@ -28,13 +21,22 @@ describe("BloomFilter", () => {
 
   describe("hashFunction", () => {
     it("Should return numerical (hashed) value of input", () => {
+      let bloom = new BloomFilter(150);
       assert.typeOf(bloom._calculateHash("Tom", 150, -1), "number");
     });
     it("Should return different values for different seeds", () => {
+      let bloom = new BloomFilter(150);
       assert.notEqual(
         bloom._calculateHash("Tom", 150, -1),
         bloom._calculateHash("Tom", 150, 1),
-        "equal!"
+        "Expected $string hashed with seed = -1 to be different than $string hashed with seed = 1"
+      );
+    });
+    it("Should return different hashes for different values using same seed", () => {
+      let bloom = new BloomFilter(150);
+      assert.notEqual(
+        bloom._calculateHash("Tom", 150, -1),
+        bloom._calculateHash("Jerry", 150, -1)
       );
     });
   });
